810092b4580b4d4cd573288cb1a7d417249e7ed5
Erivona Mega <megaerivona@gmail.com>
2025-11-05 01:14:07 +0000

    Update Data Integration run through.sql

--- scripts/gold/Data Integration run through.sql
+++ scripts/gold/Data Integration run through.sql
@@ -1,6 +1,6 @@
 /*
 --------------------------------------------------------------------------
-Explore ---> CLean/Transform
+Data Integration run through for Gold layer 
 --------------------------------------------------------------------------
 
 							-->STAR SCHEMA<--
@@ -26,9 +26,9 @@ Objectives
 		 - Create views from Data transformations
 */
 
-----------------------------------------------------
--- Customer Object 
-----------------------------------------------------
+-----------------------------------------------------
+-- CUSTOMER Object							DIMENSION
+-----------------------------------------------------
 SELECT
 	ci.cst_id,
 	ci.cst_key,
@@ -66,8 +66,8 @@ ORDER BY 1,2
 		-- it is CRM in this case
 		-- So the value from the CRM customer table is the correct one in conflicting values situation
 SELECT DISTINCT
-	ci.cst_gndr,
-	ca.gen,
+	ci.cst_gndr ,
+	ca.gen ,
 	CASE 
 		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender info when available
 		ELSE COALESCE(ca.gen, 'n/a')				-- Use ca.gen from ERP table when gender form CRM not available, and if that is also nulll print 'n/a'
@@ -77,4 +77,87 @@ LEFT JOIN silver.erp_cust_az12 ca
 	ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 	ON ci.cst_key = la.cid
-ORDER BY 1,2		
+ORDER BY 1,2;
+
+
+-- Final Customer Data Object Transfromation
+	-- Create View 
+	-- Use Naming Convention to Rename columns for Business Context
+	-- Order the columns business context manner
+	-- Label table as Dimension (Descriptive, Qualitative)
+	-- For Dimensions,generate Surrogate Keys (System generated unique identifier assigned to each record in a table)
+		-- Use Window function to generate surrogate key
+CREATE VIEW gold.dim_customers AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name ,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE 
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the Master for gender info when available
+		ELSE COALESCE(ca.gen, 'n/a')				-- Use ca.gen from ERP table when gender form CRM not available, and if that is also nulll print 'n/a'
+	END AS gender,
+	ca.bdate AS birth_date,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+	ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+	ON ci.cst_key = la.cid;
+
+
+-----------------------------------------------------
+-- PRODUCT Object							DIMENSION
+-----------------------------------------------------
+-- Create View 
+	-- Use Naming Convention to Rename columns for Business Context
+	-- Order the columns business context manner
+	-- Label table as Dimension (Descriptive, Qualitative)
+	-- For Dimensions,generate Surrogate Keys
+		-- Use Window function to generate surrogate key
+CREATE VIEW gold.dim_product AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY pr.prd_start_dt,pr.prd_key) AS product_key,
+	pr.prd_id AS product_id,
+	pr.prd_key AS product_number, -- renamed to product number to follow patter of naming convention as done for 'Customer Dimension'
+	pr.prd_nm AS product_name,
+	pr.cat_id AS category_id,
+	px.cat AS category,
+	px.subcat AS subcategory,
+	px.maintenance AS maintenance,
+	pr.prd_cost AS product_cost,
+	pr.prd_line AS product_line,
+	pr.prd_start_dt AS product_start_date
+FROM silver.crm_prd_info pr
+LEFT JOIN silver.erp_px_cat_g1v2 px
+	ON pr.cat_id = px.id
+WHERE pr.prd_end_dt IS NULL			-- Filter out historic data | as was calulated in transformation of silver layer
+
+
+-----------------------------------------------------
+-- SALES Object									FACT
+-----------------------------------------------------
+-- Create View 
+	-- Use Naming Convention to Rename columns for Business Context
+	-- Order the columns business context manner
+	-- Label table as Fact (Multiple IDs, Measures and Numbers, Quantitative)
+	-- Get surrogate key form dimensions in gold layer using JOINS
+CREATE VIEW gold.fact_sales AS
+SELECT 
+sd.sls_ord_num AS order_number,
+pr.product_key, -- Product Surrogate Key
+cu.customer_key, -- Customer Dimension Surrogate Key
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shipping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales_amount,
+sd.sls_quantity AS quantity,
+sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_customers cu
+	ON sd.sls_cust_id = cu.customer_number -- 'cst_key' renamed to customer number for gold layer dimension
+LEFT JOIN gold.dim_product pr
+	ON sd.sls_prd_key = pr.product_number; -- 'prd_key' renamed to product number for gold layer dimension
